ec1fc9d5d8694d7db7ca7f6a3bb61cc347341fad
dineshvaishnav8890 <142964391+dineshvaishnav8890@users.noreply.github.com>
2025-05-13 22:00:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Team This is My Second Task For Elevate Labs"
+    return "Hello DevOps!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
